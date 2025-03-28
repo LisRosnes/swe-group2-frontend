386472f6378ebe510635f952fbc4a80eabd3f4f9
Elisa Rosnes <elisarosnes@gmail.com>
2025-03-28 16:59:06 -0400

    additional file structure setup

---
+++ src/components/ProfilePage.js
@@ -0,0 +1,16 @@
+// Profile Page Component
+    // Profile summary comp
+        // personal info (editable form)
+            // name
+            // email
+            // bio
+        // community summary
+            // total reviews
+            // total teams
+    // my reviews
+        // game | rating | review text
+
+    // my teams
+        // team name | team description | team members
+
+
